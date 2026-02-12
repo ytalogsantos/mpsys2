1eefe505c9423dbc0f668a89fc1dae0660b40398
Ytalo Santos <ytalos762@gmail.com>
2026-02-12 09:57:55 -0300

    removing unnecessary code line

--- src/tools/input-filter.ts
+++ src/tools/input-filter.ts
@@ -1,7 +1,6 @@
 function InputFilter(obj: object): object {
     const filteredInput = {};
-    let empty: boolean = true;
-
+    
     for (const [key, value] of Object.entries(obj)) {
         if (value) {
             filteredInput[key as keyof Object] = value;
