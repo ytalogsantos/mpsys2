4c5fdb596191a3338fd980d255cb839627d5540a
Ytalo Santos <ytalos762@gmail.com>
2026-02-17 03:56:24 -0300

    starting profile routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { users } from "./user-routes.js";
+import { profiles } from "./profile-routes.js";
 import type { Express, Request, Response } from "express";
 
 const routes = (app: Express) => {
     app.route("/").get((req: Request, res: Response) => res.status(200).send("Hello server."));
     app.use(users);
+    app.use(profiles);
 }
 
 export { routes };
---
+++ src/routes/profile-routes.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import { ProfileController } from "../controllers/profile-controller.js";
+import { ProfileService } from "../services/profile-service.js";
+
+const service = new ProfileService();
+const controller = new ProfileController(service);
+const profiles = express.Router();
+
+profiles.post("/profiles", controller.create);
+
+export { profiles };
